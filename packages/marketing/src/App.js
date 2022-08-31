edcfa16e52d6f216b0ca7d72c6e2c3715ed20b89
Gabriel Godoy <gabrielgodoyon@gmail.com>
2022-08-31 17:51:40 -0300

    add mui prefix

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+})
+
 export default () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/pricing" element={<Pricing />} />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/pricing" element={<Pricing />} />
+            <Route path="/" element={<Landing />} />
+          </Routes>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
   )
 }
